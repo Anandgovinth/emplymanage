7edefc2719b75f8f8d6bc3c0e8d7d216109b966f
Anandgovinth <96039287+Anandgovinth@users.noreply.github.com>
2022-07-11 09:24:04 +0530

    Delete Emplyrepo.java

--- Emplyrepo.java
+++
@@ -1,9 +0,0 @@
-package com.bluetree.Emplyreg.repository;
-
-import com.bluetree.Emplyreg.model.Emply;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Emplyrepo<id> extends JpaRepository<Emply, Integer>{
-}
